3ceb44ce8b77be14ae2e8fd8a83d5f25b7ddee7b
Oscar <oscar.nesis@gmail.com>
2016-11-18 00:31:58 +0100

    Improved graphic and contents

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <link href="https://plus.google.com/+OscarFanelli" rel="publisher">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-    <link href="/css/css.min.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="/css/css.css" media="screen" rel="stylesheet" type="text/css">
     <link href="http://www.oscarfanelli.com/" rel="canonical">
 
     <!-- Google Tag Manager -->
@@ -72,16 +72,8 @@
     <div class="block block-spaced">
         <div>
             <p>
-                In my spare time, I work (and I love it).
-            </p>
-            <p>
-                I don't have time to create a stunning portfolio with many animations that let you say
-                <? $rand = rand(1,8); ?>
-                <span class="colorful colorful--complex colorful--color-<?=$rand?> colorful--effect-oscar" data-round="<?=$rand?>">
-                    <span class="colorful__text">WOW!</span>
-                    <span class="colorful__helper colorful__helper--first"><img src="/images/splash_1.svg#color-<?=$rand?>" /></span>
-                    <span class="colorful__helper colorful__helper--second"><img src="/images/splash_2.svg#color-<?=$rand?>" /></span>
-                </span>
+                In my spare time, I work.<br />
+                And I love it.
             </p>
             <p>
                 Let's skip pleasantries and go straight to the point.
@@ -90,12 +82,11 @@
     </div>
 
     <div class="block block-spaced cloud">
-        <h3>It's not a bug</h3>
         <div>
-            <span class="change" data-hidden="I'm very good at it!">Team Management</span>
-            <span class="change" data-hidden="Seriously?">Walk on water</span>
+            <span class="change" data-hidden="I'm very good at it.">Team Management</span>
             <span class="change" data-hidden="PHP">Zend Framework 2</span>
             <span class="change" data-hidden="MySQL">Doctrine 2</span>
+            <span class="change" data-hidden="Seriously?">Walk on water</span>
             <span class="change" data-hidden="Javascript">jQuery</span>
             <span class="change" data-hidden="Don't you know?">React</span>
             <span class="change" data-hidden="Ruby">Rails</span>
@@ -105,70 +96,49 @@
             <span class="change" data-hidden="Not only development">Troubleshooting in critical situations</span>
             <span class="change" data-hidden="Team managemnt">Asana</span>
             <span class="change" data-hidden="My life">Todoist</span>
+            <span class="change" data-hidden="Also in local">Docker</span>
             <span class="change" data-hidden="Versioning">Git</span>
         </div>
     </div>
 
-    <div class="block block-spaced block-double life">
-        <h3>Live as Bicentennial Man</h3>
+    <div class="block block-double life">
         <div>
-            <div>mar 2015 -> today</div>
-            <div>CTO and Frontend Developer at <a href="https://www.uala.it" target="_blank">Uala</a> <small>(Immobiliare.it group)</small></div>
-        </div>
-        <div>
-            <div>oct 2012 -> today</div>
-            <div>Full Stack Developer at <a href="http://www.tomshw.it" target="_blank">Tom's Hardware</a> <small>(partner Repubblica, l'Espresso)</small></div>
-        </div>
-        <div>
-            <div>2013 -> 2015</div>
-            <div>Full Stack Developer at <a href="http://www.copernicum.it" target="_blank">Copernicum</a></div>
-        </div>
-        <div>
-            <div>2013 -> 2014</div>
-            <div>Full Stack Developer at <a href="http://www.truemetal.it" target="_blank">TrueMetal</a></div>
-        </div>
-        <div>
-            <div>2009 -> 2013</div>
-            <div>Degree in <em>Ingegneria dell'Informazione e Organizzazione di Impresa</em> at <a href="http://www.unitn.it/" target="_blank">Trento</a></div>
-        </div>
-        <div>
-            <div>apr 2007 -> 2015</div>
-            <div>Director and Developer at <a href="http://www.gamempire.it" target="_blank">Gamempire</a></div>
-        </div>
-        <div>
-            <div>2007, 2008, 2009</div>
-            <div>Winner of <a href="http://www.mediashow.cc/" target="_blank">International Olympics of Multimedia</a> (3°, 2° and 4° position)</div>
+            <div>
+                <div>mar 2015 -> today</div>
+                <div>CTO and Frontend Developer at <a href="https://www.uala.it" target="_blank">Uala</a> <small>(Immobiliare.it group)</small></div>
+            </div>
+            <div>
+                <div>oct 2012 -> today</div>
+                <div>Full Stack Developer at <a href="http://www.tomshw.it" target="_blank">Tom's Hardware</a> <small>(partner Repubblica, l'Espresso)</small></div>
+            </div>
+            <div>
+                <div>2013 -> 2015</div>
+                <div>Full Stack Developer at <a href="http://www.copernicum.it" target="_blank">Copernicum</a></div>
+            </div>
+            <div>
+                <div>2013 -> 2014</div>
+                <div>Full Stack Developer at <a href="http://www.truemetal.it" target="_blank">TrueMetal</a></div>
+            </div>
+            <div>
+                <div>2009 -> 2013</div>
+                <div>Degree in <em>Ingegneria dell'Informazione e Organizzazione di Impresa</em> at <a href="http://www.unitn.it/" target="_blank">Trento</a></div>
+            </div>
+            <div>
+                <div>apr 2007 -> 2015</div>
+                <div>Director and Developer at <a href="http://www.gamempire.it" target="_blank">Gamempire</a></div>
+            </div>
+            <div>
+                <div>2007, 2008, 2009</div>
+                <div>Winner of <a href="http://www.mediashow.cc/" target="_blank">International Olympics of Multimedia</a> (3°, 2° and 4° position)</div>
+            </div>
         </div>
     </div>
 
+    <div class="block block-spaced block-spaced-half"></div>
+
     <footer class="block">
         <div>
-            <h3>Contact</h3>
             <ul>
-                <li>
-                    <a href="mailto:oscar.fanelli@gmail.com" target="_blank">
-                        <span>Email</span>
-                        <span class="s2 icon icon-email"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="skype:gamempire" target="_blank">
-                        <span>Skype</span>
-                        <span class="s2 icon icon-skype"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="tel:00393388696167" target="_blank">
-                        <span>Phone</span>
-                        <span class="s2 icon icon-phone"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.facebook.com/oscar.fanelli" target="_blank">
-                        <span>Facebook</span>
-                        <span class="s2 icon icon-facebook"></span>
-                    </a>
-                </li>
                 <li>
                     <a href="https://www.linkedin.com/in/oscarfanelli" target="_blank">
                         <span>LinkedIn</span>
@@ -179,6 +149,30 @@
                     <a href="https://github.com/pensiero" target="_blank">
                         <span>GitHub</span>
                         <span class="s2 icon icon-github"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:oscar.fanelli@gmail.com" target="_blank">
+                        <span>Email</span>
+                        <span class="s2 icon icon-email"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="tel:00393388696167" target="_blank">
+                        <span>Phone</span>
+                        <span class="s2 icon icon-phone"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="skype:gamempire" target="_blank">
+                        <span>Skype</span>
+                        <span class="s2 icon icon-skype"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/oscar.fanelli" target="_blank">
+                        <span>Facebook</span>
+                        <span class="s2 icon icon-facebook"></span>
                     </a>
                 </li>
                 <li>
