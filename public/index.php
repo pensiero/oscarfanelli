073410af00780d18cdb126f0496889130515a787
pensiero <oscar.nesis@gmail.com>
2021-05-03 08:34:08 +0200

    Typos

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <meta property="og:site_name" content="Oscar Fanelli">
     <meta property="og:type" content="website">
-    <meta property="og:locale" content="it_IT">
+    <meta property="og:locale" content="en_US">
     <meta name="apple-touch-fullscreen" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -64,11 +64,11 @@
         <div class="block block-inverse" id="scrollbar">
             <div>
                 <h2>Head of Engineering, Berlin</h2>
-                <h2 class="hidden">Well... i'm tired of following this page!<br />👋 Bye bye in <span class="pixel-counter">0</span> pixels!</h2>
+                <h2 class="hidden">Well... I'm tired of following this page!<br />👋 Bye bye in <span class="pixel-counter">0</span> pixels!</h2>
             </div>
         </div>
         <a href="https://github.com/pensiero/oscarfanelli" target="_blank" id="see-this-on-github">
-            This site is public on
+            This website is public on
             <svg aria-hidden="true" class="octicon octicon-mark-github" height="24" width="24" version="1.1" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>
         </a>
     </header>
@@ -90,7 +90,7 @@
 
     <div class="block block-spaced cloud">
         <div>
-            <span class="change" data-hidden="Carrer Development">People Management</span>
+            <span class="change" data-hidden="Career Development">People Management</span>
             <span class="change" data-hidden="Love It">International Growth</span>
             <span class="change" data-hidden="Tuckman's Model">Teams Development</span>
             <span class="change" data-hidden="OKRs">KPIs</span>
